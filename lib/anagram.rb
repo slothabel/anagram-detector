c0e2096d9397182d0c607d520b122f6cf30d62bf
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-06-18 16:15:14 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+    attr_accessor :word
+    
+def initialize(word)
+    @word = word
+end
+
+def match(ana_array)
+    ana_array.select {|i| i.split("").sort == @word.split("").sort}
+end
+
+end
