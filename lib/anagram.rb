a0629b8ebc1789c52b1475e1dc67ab4b56f45f77
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-06-18 15:51:28 -0700

    commit tree b1415abde7d7ffa13a4367f307f98b95b2a0b6b7

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+    attr_accessor :word
+    ARRAY = []
+
+def initialize(word)
+    @word = word
+end
+
+def match(ana_array)
+    ana_array.select {|i| i.split("").sort == @word.split("").sort}
+end
+
+end
